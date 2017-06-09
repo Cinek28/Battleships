923b0fad5ea52cbf5374092d58152de528167294
Cinek28 <marcinb280@op.pl>
2017-06-09 15:06:22 +0200

    Added Model files

--- Model/Board.java
+++ Model/Board.java
@@ -2,8 +2,10 @@ package Model;
 
 
 public class Board {
+	private static final int SIZE = 15;
 	private boolean isVisible = true;
 	private Status boardState [][];
 	private int noOfShipsOnBoard = 0;
+	
 	
 }
